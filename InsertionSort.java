ba654abcb204ca33f6693e2cdd03150954d7f4ed
KanishkaKanungo <102153991+KanishkaKanungo@users.noreply.github.com>
2024-01-08 23:22:24 +0530

    Add files via upload

---
+++ InsertionSort.java
@@ -0,0 +1,24 @@
+import java.util.Arrays;
+
+public class InsertionSort {
+    public static void main(String[] args) {
+        int[] arr = {4,3,1,2,6,87};
+        System.out.println(Arrays.toString(insertion(arr)));
+
+    }
+    static int[] insertion(int[] arr){
+        for (int i = 0; i < arr.length-1; i++) {
+            for (int j = i+1; j>0 ; j--) {
+                if(arr[j]<arr[j-1]){
+                 int temp = arr[j];
+                 arr[j] = arr[j-1];
+                 arr[j-1] = temp;
+                }
+                else{
+                    break;
+                }
+            }
+        }
+        return arr;
+    }
+}
