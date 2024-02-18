b811a608ee4533c0d669832060007a23fcf60063
Kanishka Kanungo <kanungokanishka@gmail.com>
2024-02-18 16:30:27 +0530

    Easy method to find no of digits

---
+++ Bit-Manipulation/NoOfDigits.java
@@ -0,0 +1,8 @@
+public class NoOfDigits {
+    public static void main(String[] args) {
+        int n = 6;
+        int b = 2;
+        int ans = (int) (Math.log(6)/Math.log(2)) +1;
+        System.out.println(ans);
+    }
+}
