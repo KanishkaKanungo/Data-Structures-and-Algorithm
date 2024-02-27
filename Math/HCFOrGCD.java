62e3194bca01afd328a485a390624bf82dc259f5
Kanishka Kanungo <kanungokanishka@gmail.com>
2024-02-27 12:52:12 +0530

    Program to find HCF of a number

---
+++ Math/HCFOrGCD.java
@@ -0,0 +1,11 @@
+public class HCFOrGCD{
+  public static void main(String[] args){
+   System.out.println(check(105,224));
+  }
+  static int check(int a,int b){
+    if(a==0){
+      return b;
+     }
+    return check(b%a,a);
+  }
+}
