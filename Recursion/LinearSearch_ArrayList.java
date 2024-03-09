97a525215f007f8f4dc3263a419b521c393c8bbc
Kanishka Kanungo <kanungokanishka@gmail.com>
2024-03-09 17:58:48 +0530

    Linear Search through ArrayList

---
+++ Recursion/LinearSearch_ArrayList.java
@@ -0,0 +1,30 @@
+import java.util.ArrayList;
+
+public class LinearSearch_ArrayList {
+    public static void main(String[] args) {
+        int[] arr = {1,2,3,5,6,6};
+        ArrayList<Integer> list = new ArrayList<>();
+        System.out.println(search1(arr,6,0));
+    }
+    static ArrayList<Integer> search(int[] arr,int target,int i,ArrayList<Integer> list){
+        if(i==arr.length){
+            return list;
+        }
+        if(arr[i]==target){
+            list.add(i+1);
+        }
+        return search(arr,target,i+1,list);
+    }
+    static ArrayList<Integer> search1(int[] arr,int target,int i){
+        ArrayList<Integer> list = new ArrayList<>();
+        if(i==arr.length){
+            return list;
+        }
+        if(arr[i]==target){
+            list.add(i);
+        }
+        ArrayList<Integer> ans = search1(arr,target,i+1);
+        list.addAll(ans);
+        return list;
+    }
+}
