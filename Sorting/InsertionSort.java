00655bfacbdc18eec9b631266a551bdc6a1adc75
KanishkaKanungo <102153991+KanishkaKanungo@users.noreply.github.com>
2024-03-25 23:19:14 +0530

    Update InsertionSort.java

--- Sorting/InsertionSort.java
+++ Sorting/InsertionSort.java
@@ -10,7 +10,7 @@ public class InsertionSort {
         for (int i = 0; i < arr.length-1; i++) {
             for (int j = i+1; j>0 ; j--) {
                 if(arr[j]<arr[j-1]){
-                 int temp = arr[j];
+                 int temp = arr[j]; 
                  arr[j] = arr[j-1];
                  arr[j-1] = temp;
                 }
