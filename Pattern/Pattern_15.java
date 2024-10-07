884e8e9e4d933a4e848c025e06227c502f327261
Kanishka Kanungo <kanungokanishka@gmail.com>
2024-10-07 19:11:20 +0530

    Pattern 15 and 16 added

---
+++ Pattern/Pattern_15.java
@@ -0,0 +1,17 @@
+/* 15.
+         1
+         2 2
+         3 3 3
+* */
+
+public class Pattern_15 {
+    public static void nTriangle(int n) {
+        // Write your code here
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=i;j++){
+                System.out.print(i+" ");
+            }
+            System.out.println();
+        }
+    }
+}
